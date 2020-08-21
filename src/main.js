74532dea6c87b695f6ceabc8d1bd3669414418aa
Airubby <ai_rubby@qq.com>
2020-08-21 19:06:50 +0800

    模拟告警设置

--- src/main.js
+++ src/main.js
@@ -2,11 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import {router} from './router/index'
 import store from './store/index'
-
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import './assets/css/basic.less'
 
 Vue.prototype.$store=store
-
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 function init() {
